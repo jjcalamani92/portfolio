5e81e2e0d77d35549b1b7f4b2f9583ce5c49e7a7
Jesus Calamani <jesuscalamani92@gmail.com>
2023-06-19 16:10:43 -0400

    message

--- src/lib/articles.ts
+++ src/lib/articles.ts
@@ -8,7 +8,7 @@ export const getArticleById = async (id: string):Promise<Article> => {
   return res.json();
 };
 export const getArticlesByParentId = async (id: string):Promise<Article []> => {
-  const res = await fetch(`${url}/api/${v}/${type}/query/articles/article/parentId?id=${id}`, {cache: 'no-store'});
+  const res = await fetch(`${url}/api/${v}/${type}/query/articles/article/parentId?id=${id}`,);
 
   return res.json();
 };
