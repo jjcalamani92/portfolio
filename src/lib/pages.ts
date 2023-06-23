e6d27ad6db74de914f5ab061466314758e91dc33
Jesus Calamani <jesuscalamani92@gmail.com>
2023-06-23 08:45:17 -0400

    message

--- src/lib/pages.ts
+++ src/lib/pages.ts
@@ -1,24 +1,36 @@
 import { Page } from "../interfaces/page";
 import { uid, url, type, v } from "../utils";
 
-export async function getPagesNavigation( ) {
-  const res = await fetch(`${url}/api/${v}/${type}/query/pages/page/navigation?siteId=${uid}`, {cache: 'no-cache'});
+export async function getPagesNavigation() {
+  const res = await fetch(
+    `${url}/api/${v}/${type}/query/pages/page/navigation?siteId=${uid}`,
+    { cache: "no-cache" }
+  );
   return res.json();
 }
-export async function getPagesByParentId( ) {
-  const res = await fetch(`${url}/api/${v}/${type}/query/pages/page/parentId?id=${uid}`,);
+export async function getPagesByParentId() {
+  const res = await fetch(
+    `${url}/api/${v}/${type}/query/pages/page/parentId?id=${uid}`
+  );
   return res.json();
 }
 
-export async function getPageBySlug(slug: string):Promise<Page> {
-  const res = await fetch(`${url}/api/${v}/${type}/query/pages/page/${uid}/${slug}`,);
+export async function getPageBySlug(slug: string): Promise<Page> {
+  const res = await fetch(
+    `${url}/api/${v}/${type}/query/pages/page/${uid}/${slug}`,
+    { next: { revalidate: 60 } }
+  );
   return res.json();
 }
 export async function getPageSeoBySlug(slug: string) {
-  const res = await fetch(`${url}/api/${v}/${type}/query/pages/page/${uid}/${slug}`);
+  const res = await fetch(
+    `${url}/api/${v}/${type}/query/pages/page/${uid}/${slug}`
+  );
   return res.json();
 }
-export async function getPagesBySiteId():Promise<Page[]> {
-  const res = await fetch(`${url}/api/${v}/${type}/query/pages/page/siteId?id=${uid}`, );
+export async function getPagesBySiteId(): Promise<Page[]> {
+  const res = await fetch(
+    `${url}/api/${v}/${type}/query/pages/page/siteId?id=${uid}`
+  );
   return res.json();
 }
